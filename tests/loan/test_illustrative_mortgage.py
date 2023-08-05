ec73378187b246546e5320f6b0264e2933e1367c
Sam Thorold <sam.thorold@gmail.com>
2023-08-05 18:16:55 +0100

    RepaymentSchedule interest and total

--- tests/loan/test_illustrative_mortgage.py
+++ tests/loan/test_illustrative_mortgage.py
@@ -15,15 +15,19 @@ def test_illustrative_mortgage() -> None:
         LoanTerm(rate=0.0519, term=24),
         LoanTerm(rate=0.0779, term=1),
     ]
-    loan_periods = IllustrativeMortgage(
+
+    expected_total = 529_653
+
+    repayment = IllustrativeMortgage(
         start_value=start_value,
         loan_terms=loan_terms,
         repayment_period=repayment_period,
     ).calculate()
-    assert len(loan_periods) == 300
-    assert round(loan_periods[0].payment, 2) == 1429.71
-    assert round(loan_periods[24].payment, 2) == 1794.71
-    assert round(sum(lp.payment for lp in loan_periods.periods)) == 529653
+    assert len(repayment) == 300
+    assert round(repayment[0].payment, 2) == 1429.71
+    assert round(repayment[24].payment, 2) == 1794.71
+    assert round(repayment.interest) == (expected_total - start_value)
+    assert round(repayment.total) == expected_total
 
 
 def test_illustrative_mortgage_3_terms() -> None:
@@ -34,16 +38,17 @@ def test_illustrative_mortgage_3_terms() -> None:
         LoanTerm(rate=0.0675, term=36),
         LoanTerm(rate=0.0849, term=1),
     ]
-    loan_periods = IllustrativeMortgage(
+
+    expected_total = 541_800
+
+    repayment = IllustrativeMortgage(
         start_value=start_value,
         loan_terms=loan_terms,
         repayment_period=repayment_period,
     ).calculate()
-    assert len(loan_periods) == 300
-    assert round(loan_periods[0].payment, 2) == 1393.25
-    assert round(loan_periods[24].payment, 2) == 1641.37
-    assert round(loan_periods[24 + 36].payment, 2) == 1871.97
-    assert (
-        abs(round(sum(lp.payment for lp in loan_periods.periods)) - (542939 - 1139))
-        <= 1
-    ), round(sum(lp.payment for lp in loan_periods.periods))
+    assert len(repayment) == 300
+    assert round(repayment[0].payment, 2) == 1393.25
+    assert round(repayment[24].payment, 2) == 1641.37
+    assert round(repayment[24 + 36].payment, 2) == 1871.97
+    assert abs(round(repayment.interest) - (expected_total - start_value)) <= 1
+    assert abs(round(repayment.total) - expected_total) <= 1, repayment.total
